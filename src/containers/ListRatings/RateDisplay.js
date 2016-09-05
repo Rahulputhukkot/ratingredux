89dbf81927eca126b8be9911db7b7940d4ed854d
yadhu <yadhu@qburst.com>
2016-09-05 19:48:27 +0530

    next

--- src/containers/ListRatings/RateDisplay.js
+++ src/containers/ListRatings/RateDisplay.js
@@ -9,8 +9,26 @@ export default class RateDisplay extends Component {
     const displayNodes = this.props.data.map((rates) => {
       return (<Display rating={rates.rating} description={rates.description} email ={rates.email} key={rates.id} timestamp={rates.id}/>);
     });
+    const style = require('./ListRatings.scss');
     return (
       <div className="DisplayList">
+        <div className={style.display}>
+          <table>
+            <tr>
+              <div className={style.sameRow} >
+              <div className={style.first}>
+                <h4>Email</h4>
+              </div>
+              <div className={style.second}>
+                <h4>Rating</h4>
+              </div>
+              <div className={style.third}>
+                <h4>Description</h4>
+              </div>
+            </div>
+            </tr>
+          </table>
+        </div>
         {displayNodes}
       </div>
     );
